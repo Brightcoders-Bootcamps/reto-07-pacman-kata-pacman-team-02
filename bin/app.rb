7d59fdbf1dc5d40b9578c4874539fc9c54e7e67f
CarlosSe <carlos.seggch@gmail.com>
2020-08-05 19:55:02 -0500

    Increasing rubycritics score.
    Co-authored-by: Ana Laura Arreola <laurarreola.ana@gmail.com>

--- bin/app.rb
+++ bin/app.rb
@@ -1,11 +1,15 @@
+# frozen_string_literal: true
+
 require_relative 'grid'
 require_relative 'pacman'
 require_relative 'ghosts'
 require 'io/console'
+
+# This class start the game
 class App
   def initialize
     @grid = Grid.new(20, 11)
-    @pacman = Pacman.new(7, 9, :left)
+    @pacman = Pacman.new(7, 9, 1)
     @score = 0
     @level = 1
   end
@@ -16,21 +20,7 @@ class App
     @ghost2 = Ghosts.new(5, 9, @complete_grid)
     @ghost3 = Ghosts.new(5, 10, @complete_grid)
     @ghost4 = Ghosts.new(5, 11, @complete_grid)
-    # @ghost1.show_ghost(@complete_grid)
-    # @ghost2.show_ghost(@complete_grid)
-    # @ghost3.show_ghost(@complete_grid)
-    # @ghost4.show_ghost(@complete_grid)
-    # #Ghosts.open_ghost_container(@complete_grid)
-    # @ghost4.open_ghost_container(@complete_grid)
-    # @complete_grid[4][8] = 6
-    # @complete_grid[4][9] = 6
-    # @complete_grid[4][10] = 6
-    # @complete_grid[5][8] = 8
 
-    # @complete_grid = @ghost1.position_ghosts(@complete_grid)
-    # @complete_grid = @ghost2.position_ghosts(@complete_grid)
-    # @complete_grid = @ghost3.position_ghosts(@complete_grid)
-    # @complete_grid = @ghost4.position_ghosts(@complete_grid)
     char = 'W'
     @test = 'W'
     @jump = false
@@ -45,90 +35,31 @@ class App
         puts 'Game Over'
         break
       end
-      unless @jump
-        puts "You press #{char}"
-        system('cls') || system('clear')
-        puts "\tPacman game"
-        puts 'Level 1'
-        puts "Score #{@score}"
-        @grid.show_maze_two(@pacman)
-        sleep(0.1)
-        system('cls') || system('clear')
-        puts "\tPacman game"
-        puts 'Level #{@level}'
-        puts "Score #{@score}"
-        @grid.show_maze(@pacman)
-      end
+      print_board unless @jump
       case @test
       when 'W'
-        puts "Ingresa enter para iniciar"
+        puts 'Ingresa enter para iniciar'
       when 'A'
         @score = @pacman.move(1, @complete_grid, @score)
         @test = 'A'
-        puts "You press #{char}"
-        system('cls') || system('clear')
-        puts "\tPacman game"
-        puts 'Level 1'
-        puts "Score #{@score}"
-        @grid.show_maze_two(@pacman)
-        sleep(0.1)
-        system('cls') || system('clear')
-        puts "\tPacman game"
-        puts 'Level 1'
-        puts "Score #{@score}"
-        @grid.show_maze(@pacman)
+        print_board
       when 'B'
         @score = @pacman.move(3, @complete_grid, @score)
         @test = 'B'
-        puts "You press #{char}"
-        system('cls') || system('clear')
-        puts "\tPacman game"
-        puts 'Level 1'
-        puts "Score #{@score}"
-        @grid.show_maze_two(@pacman)
-        sleep(0.1)
-        system('cls') || system('clear')
-        puts "\tPacman game"
-        puts 'Level 1'
-        puts "Score #{@score}"
-        @grid.show_maze(@pacman)
+        print_board
       when 'C'
         @score = @pacman.move(2, @complete_grid, @score)
         @test = 'C'
-        puts "You press #{char}"
-        system('cls') || system('clear')
-        puts "\tPacman game"
-        puts 'Level 1'
-        puts "Score #{@score}"
-        @grid.show_maze_two(@pacman)
-        sleep(0.1)
-        system('cls') || system('clear')
-        puts "\tPacman game"
-        puts 'Level 1'
-        puts "Score #{@score}"
-        @grid.show_maze(@pacman)
+        print_board
       when 'D'
         @score = @pacman.move(4, @complete_grid, @score)
         @test = 'D'
-        puts "You press #{char}"
-        system('cls') || system('clear')
-        puts "\tPacman game"
-        puts 'Level 1'
-        puts "Score #{@score}"
-        @grid.show_maze_two(@pacman)
-        sleep(0.1)
-        system('cls') || system('clear')
-        puts "\tPacman game"
-        puts 'Level 1'
-        puts "Score #{@score}"
-        @grid.show_maze(@pacman)
+        print_board
       when 'Q'
         break
       end
       prev_value = @ghost1.intelligent_move(@pacman.position_x, @pacman.position_y, prev_value)
-      # @ghost2.intelligent_move(@complete_grid, @pacman, prev_value)
-      # @ghost3.random_move(@complete_grid, rand(1..10), rand(1..5))
-      # @ghost4.random_move(@complete_grid, rand(11..19), rand(6..10))
+
       system('stty raw -echo')
       char = begin
                STDIN.read_nonblock(1)
@@ -145,55 +76,20 @@ class App
     end
   end
 
-  # def move()
-  #   current_move = $stdin.getch
-  #   case current_move
-  #   when 'A'
-  #       puts "up TEST "
-  #       @pacman.move(1, @complete_grid)
-  #       puts "#{@pacman.position_x} - #{@pacman.position_y} - #{@complete_grid[@pacman.position_x][@pacman.position_y]}"
-  #       system('cls') || system('clear')
-  #       @grid.show_maze_two(@pacman)
-  #       sleep(0.5)
-  #       system('cls') || system('clear')
-  #       @grid.show_maze(@pacman)
-  #       #move('A')
-  #       move
-  #   when 'B'
-  #       puts "down"
-  #       @pacman.move(3, @complete_grid)
-  #       puts "#{@pacman.position_x} - #{@pacman.position_y}"
-  #       system('cls') || system('clear')
-  #       @grid.show_maze_two(@pacman)
-  #       sleep(0.5)
-  #       system('cls') || system('clear')
-  #       @grid.show_maze(@pacman)
-  #       #move('B')
-  #       move
-  #   when 'C'
-  #       puts "right"
-  #       @pacman.move(2, @complete_grid)
-  #       puts "#{@pacman.position_x} - #{@pacman.position_y}"
-  #       system('cls') || system('clear')
-  #       @grid.show_maze_two(@pacman)
-  #       sleep(0.5)
-  #       system('cls') || system('clear')
-  #       @grid.show_maze(@pacman)
-  #       #move('C')
-  #       move
-  #   when 'D'
-  #       puts "left"
-  #       @pacman.move(4, @complete_grid)
-  #       puts "#{@pacman.position_x} - #{@pacman.position_y}"
-  #       system('cls') || system('clear')
-  #       @grid.show_maze_two(@pacman)
-  #       sleep(0.5)
-  #       system('cls') || system('clear')
-  #       @grid.show_maze(@pacman)
-  #       #move('D')
-  #       move
-  #   end
-  # end
+  def print_board
+    system('cls') || system('clear')
+    puts "\tPacman game"
+    puts 'Level 1'
+    puts "Score #{@score}"
+    @grid.show_maze(Pacman::BIG[@pacman.direction])
+    # @grid.show_maze(Pacman::BIG[@pacman.direction])
+    sleep(0.1)
+    system('cls') || system('clear')
+    puts "\tPacman game"
+    puts 'Level #{@level}'
+    puts "Score #{@score}"
+    @grid.show_maze(Pacman::MINI[@pacman.direction])
+  end
 end
 
 app = App.new
--- bin/pacman.rb
+++ bin/pacman.rb
@@ -1,59 +1,25 @@
+# frozen_string_literal: true
+
 class Pacman
   attr_reader :position_x, :position_y, :direction
+
+  MINI = {
+    TOP: 'u',
+    RIGHT: 'c',
+    DOWN: 'n',
+    LEFT: 'ͻ'
+  }.freeze
+
+  BIG = {
+    UP: 'Ս',
+    RIGHT: 'C',
+    DOWN: 'Ո',
+    LEFT: 'Ɔ'
+  }.freeze
 
   def initialize(position_x, position_y, direction)
     @position_x = position_x
     @position_y = position_y
     @direction = direction
   end
-
-  def move(direction, grid, score)
-    @direction = direction
-    prev_x = @position_x
-    prev_y = @position_y
-    if direction == 1 && (grid[@position_x - 1][@position_y] != :horizontal && grid[@position_x - 1][@position_y] != :vertical)
-      @position_x -= 1
-      end  # Arriba
-    if direction == 2 && (grid[@position_x][@position_y + 1] != :horizontal && grid[@position_x][@position_y + 1] != :vertical)
-      @position_y += 1
-      end  # Derecha
-    if direction == 3 && (grid[@position_x + 1][@position_y] != :horizontal && grid[@position_x + 1][@position_y] != :vertical)
-      @position_x += 1
-      end  # Abajo
-    if direction == 4 && (grid[@position_x][@position_y - 1] != :horizontal && grid[@position_x][@position_y - 1] != :vertical)
-      @position_y -= 1
-      end  # Izquierda
-    eats(prev_x, prev_y, grid, direction, score)
-  end
-
-  def eats(prev_x, prev_y, grid, direction, score)
-    if grid[prev_x][prev_y] == grid[@position_x][@position_y]
-      grid[@position_x][@position_y] = 7
-    elsif grid[@position_x][@position_y] == 3 && direction == 1
-      grid[prev_x][prev_y] = 6
-      score += 1 if grid[@position_x][@position_y] == 0
-      @position_x = grid.length - 2
-      grid[@position_x][@position_y] = 7
-    elsif grid[@position_x][@position_y] == 3 && direction == 3
-      score += 1 if grid[@position_x][@position_y] == 0
-      grid[prev_x][prev_y] = 6
-      @position_x = 1
-      grid[@position_x][@position_y] = 7
-    elsif grid[@position_x][@position_y] == 4 && direction == 2
-      score += 1 if grid[@position_x][@position_y] == 0
-      grid[prev_x][prev_y] = 6
-      @position_y = 1
-      grid[@position_x][@position_y] = 7
-    elsif grid[@position_x][@position_y] == 4 && direction == 4
-      score += 1 if grid[@position_x][@position_y] == 0
-      grid[prev_x][prev_y] = 6
-      @position_y = grid[0].length - 2
-      grid[@position_x][@position_y] = 7
-    else
-      score += 1 if grid[@position_x][@position_y] == 0
-      grid[@position_x][@position_y] = 7
-      grid[prev_x][prev_y] = 6
-    end
-    score
-  end
-  end
+end
